79c47dcb67a072ef6d5ea45c7f20329418c25e0c
AraceliColmenero <ara_991990@hotmail.com>
2017-03-25 15:15:29 -0600

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+# Specifies gem version of Rails to use when vendor/rails is not present
+#RAILS_GEM_VERSION = '4.2.6' unless defined? RAILS_GEM_VERSION
